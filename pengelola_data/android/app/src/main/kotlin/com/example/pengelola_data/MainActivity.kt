2a068a708af17848102fd2927c1f41158f7e2287
Ryas-Yusenda <44300598+Ryas-Yusenda@users.noreply.github.com>
2022-03-02 12:35:24 +0700

    Pengelola Data

---
+++ pengelola_data/android/app/src/main/kotlin/com/example/pengelola_data/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pengelola_data
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ pengelola_data/lib/login_page.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  String username = "";
+  String password = "";
+
+  LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+          appBar: AppBar(
+            title: Text("Login Page"),
+          ),
+          body: Column(children: [
+            _username(),
+            _password(),
+            _loginButton(context),
+          ])),
+    );
+  }
+
+  Widget _username() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: TextFormField(
+        enabled: true,
+        onChanged: (value) {
+          username = value;
+        },
+        decoration: const InputDecoration(
+          hintText: 'Username',
+          contentPadding: const EdgeInsets.all(8.0),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(8.0)),
+            borderSide: BorderSide(color: Colors.blue),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _password() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: TextFormField(
+        enabled: true,
+        onChanged: (value) {
+          password = value;
+        },
+        obscureText: true,
+        decoration: const InputDecoration(
+          hintText: 'Password',
+          contentPadding: const EdgeInsets.all(8.0),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(8.0)),
+            borderSide: BorderSide(color: Colors.blue),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _loginButton(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      width: MediaQuery.of(context).size.width,
+      child: ElevatedButton(
+        onPressed: () {
+          String text = "";
+          if (username == "ryas" && password == "123") {
+            text = "Login Success";
+          } else {
+            text = "Login Failed";
+          }
+          SnackBar snackBar = SnackBar(
+            content: Text(text),
+          );
+          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+        },
+        child: const Text('Login'),
+      ),
+    );
+  }
+}
+
