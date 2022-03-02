fb68996efdfd50df59532dd81a02af5ca45e6f7a
Ryas-Yusenda <44300598+Ryas-Yusenda@users.noreply.github.com>
2022-03-02 20:52:08 +0700

    topic_2_layouting

--- topic_2_layouting/lib/main.dart
+++ topic_2_layouting/lib/main.dart
@@ -11,26 +11,90 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Login Screen',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
         home: Scaffold(
-          appBar: AppBar(title: Text("Login Screen")),
-          body: Center(
-            child: TextFormField(
-                decoration: const InputDecoration(
-                  icon: Icon(Icons.person),
-                  hintText: 'Nama panggilan?',
-                  labelText: 'Name *',
-                ),
-                onSaved: (String? value) {},
-                validator: (String? value) {
-                  return (value != null && value.contains('@'))
-                      ? 'Do not use the @ char'
-                      : null;
-                }),
+          appBar: AppBar(
+            title: const Text("Login Screen"),
           ),
+          resizeToAvoidBottomInset: false,
+          body: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                _logo(),
+                _username(),
+                _password(),
+                _loginButton(context),
+                _resetPass(),
+              ]),
         ));
+  }
+
+  Widget _logo() {
+    return const FlutterLogo(
+      size: 50,
+      style: FlutterLogoStyle.markOnly,
+      curve: Curves.bounceInOut,
+      duration: Duration(seconds: 5),
+    );
+  }
+
+  Widget _username() {
+    return Container(
+      padding: const EdgeInsets.fromLTRB(30, 80, 30, 0),
+      child: TextFormField(
+        enabled: true,
+        decoration: const InputDecoration(
+          hintText: 'Email',
+          contentPadding: EdgeInsets.all(20.0),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(30)),
+            borderSide: BorderSide(color: Colors.blue),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _password() {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+      child: TextFormField(
+        enabled: true,
+        obscureText: true,
+        decoration: const InputDecoration(
+          hintText: 'Password',
+          contentPadding: EdgeInsets.all(20.0),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(30)),
+            borderSide: BorderSide(color: Colors.blue),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _loginButton(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+      width: 1000,
+      child: ElevatedButton(
+        child: const Text("Log In"),
+        onPressed: () {}, // Respon ketika button ditekan
+      ),
+    );
+  }
+
+  Widget _resetPass() {
+    return Container(
+        child: TextButton(
+          child: const Text("Forgot Password ?",
+          style: TextStyle(fontSize: 16, color: Colors.black54)),
+      onPressed: () {}, // Respon ketika button ditekan
+    ));
   }
 }
